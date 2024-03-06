6a3ad935c511c68110b92881cc23513248bb8d64
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-06 19:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if ((str.length===2 && str[0]!==str[1])|| str.length===1) {
+		return str[0];
+	}else if (str.length===0 || (str.length===2 && str[0]===str[1])) {
+		return null;
+	}
+	let i=0;
+	let j = 1;
+	let k = 2;
+	while (k<str.length) {
+		if (str[i]!==str[j] && str[j]!==str[k]) {
+			return str[j];
+		}
+		i++;
+		j++;
+		k++;
+	}
+	if (str[str.length-2]!==str[str.length-1]) {
+		return str[str.length-1];
+	}else{
+		return null;
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
